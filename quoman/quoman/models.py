c037510aa7c42f9ec72b162d74d066ac58a0367e
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2020-05-13 11:54:32 -0400

    Agregado campos descriptivos de ciertos campos para mejora la experiencia de usuario

--- quoman/quoman/models.py
+++ quoman/quoman/models.py
@@ -6,10 +6,10 @@ from .constants import IGV
 
 class Config(models.Model):
     logo = models.ImageField('Logo de la empresa', upload_to='config', blank=True, null=True)
-    ruc = models.CharField('RUC', max_length=12, blank=True)
+    ruc = models.CharField('RUC', help_text='Ingrese el Registro Único de Contribuyentes del Perú', max_length=12, blank=True)
     razon_social = models.CharField('Razón Social', max_length=120, default='')
     direccion = models.CharField('Dirección', max_length=120, blank=True)
-    igv = models.DecimalField('IGV', max_digits=3, decimal_places=1, default=IGV)
+    igv = models.DecimalField('IGV', help_text='Ingrese el Impuesto General a las Ventas del Perú', max_digits=3, decimal_places=1, default=IGV)
     detraccion_texto = models.TextField('Texto en caso de detracción', blank=True)
 
     class Meta:
@@ -17,4 +17,4 @@ class Config(models.Model):
         verbose_name_plural = 'Configuración'
 
     def __str__(self):
-        return 'Configuración'
+        return u"%s" % (self.razon_social)
